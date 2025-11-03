9416d700261de8b8368ab77affc11bd6fa215a95
tobam <tobamohaqqeq84@gmail.com>
2025-11-03 02:53:34 +0000

    4th

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -20,7 +20,17 @@ using chrono::system_clock;
 
     const int NUM_RUNS = 15;
     const int NUM_OPS = 4;
-    const int NUM_STRUCTTS =3;
-    enum Op{}
+    const int NUM_STRUCTS =3;
+    enum Op {READ_OP,SORT_OP, INSERT_OP, DELEE_OP };
+    enum DS {VEC, LIST_DS, SET_DS};
+    const int K_INSERT = 2000;
+    const int COLW_LABEL = 12;
+    const int CLOW_NUM = 11;
+
+//---------function prototypes---------
+bool load_names_file(const string& filename, vector<string>& out);
+void run_one_race(const vector<string>& baseData, long long runSlice[NUM_OPS][NUM_STRUCTS]);
+
+
 
 
